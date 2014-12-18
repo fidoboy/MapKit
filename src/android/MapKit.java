a0079378e1729737e01a5bf6f6271ba67bdcc254
fidoboy <fidoboy@users.noreply.github.com>
2014-12-18 20:08:23 +0100

    Update MapKit.java

--- src/android/MapKit.java
+++ src/android/MapKit.java
@@ -1,5 +1,9 @@
 package com.phonegap.plugins.mapkit;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
